8d501931b8af98e3f286dfeb8fb1d99b8958bb36
armyofkittens <100879325+armyofkittens@users.noreply.github.com>
2022-05-03 13:28:29 -0600

    Add files via upload

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -30,3 +30,21 @@ INTO retiring_titles
 FROM unique_titles as u
 GROUP BY u.title
 ORDER BY COUNT(title) DESC;
+
+--Deliverable 2: The Employees Eligible for the Mentorship Program
+SELECT DISTINCT ON (emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+	INNER JOIN dept_emp AS de
+		ON (e.emp_no = de.emp_no)
+	INNER JOIN titles AS ti
+		ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
